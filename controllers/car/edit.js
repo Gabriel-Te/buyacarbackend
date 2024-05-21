83cae330b39fc30f743d0f082348679c887046ab
Gabriel-Te <gabrielteixeiracorrea5@gmail.com>
2024-05-21 10:51:55 -0300

    limpo o log de erro, image com mais caracteres

--- controllers/car/edit.js
+++ controllers/car/edit.js
@@ -3,12 +3,8 @@ import carModel from "../../models/carModel.js";
 const edit = async(req, res) => {
     try {
         const {car} = req.body
-        const idcar = parseInt(+req.params.idcar,)
 
-        console.log('Received car data:', car); // Log do corpo da requisição
-        console.log('Received car ID:', req.params.idcar); // Log do ID da URL
-        console.log('Parsed car ID:', idcar); // Log do ID convertido
-
+        console.log('Received car data:', car);
 
         const editedcar = await carModel.edit(car)
         res.json({
